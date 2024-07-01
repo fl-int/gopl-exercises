b679eee5bfa3f8f6994e444429b09a92c7d48663
Sergei Fedotov <sergei.fedotov.at@gmail.com>
2024-07-01 18:33:14 +0300

    Added 2.5

---
+++ ch2/ex2.5/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// todo:
+// 1. compare performance
+// 2. cover with tests
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: `go run . <int>`")
+		os.Exit(1)
+	}
+
+	x, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Usage: `go run . <int>`", err)
+		os.Exit(1)
+	}
+
+	var count int
+	for i := 0; i < 64 && x > 0; i++ {
+		x = x & (x - 1)
+		count++
+	}
+
+	fmt.Println(count)
+}
